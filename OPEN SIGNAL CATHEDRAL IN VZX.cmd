fa263da1f7346a98a8133aba20ac2f9563bae4d5
Jobeous_II <Joe@Joepro.ai>
2026-08-23 18:03:07 -0400

    fix: make VZX launcher handle Program Files path

--- OPEN SIGNAL CATHEDRAL IN VZX.cmd
+++ OPEN SIGNAL CATHEDRAL IN VZX.cmd
@@ -2,13 +2,26 @@
 setlocal
 set "SIGNAL_VZX_EXE=C:\Program Files (x86)\Steam\steamapps\common\VZX Creative\vzx_artiste.exe"
 
-if not exist "%SIGNAL_VZX_EXE%" (
-  echo VZX Creative was not found at:
-  echo %SIGNAL_VZX_EXE%
+if exist "%SIGNAL_VZX_EXE%" goto launch
+
+echo VZX Creative was not found at:
+echo "%SIGNAL_VZX_EXE%"
+echo.
+echo Edit SIGNAL_VZX_EXE in this launcher if your Steam library is elsewhere.
+pause
+exit /b 1
+
+:launch
+
+pushd "%~dp0"
+"%SIGNAL_VZX_EXE%" -data_path "%~dp0vzx" -state signal-cathedral-v02
+set "SIGNAL_VZX_EXIT=%ERRORLEVEL%"
+popd
+
+if not "%SIGNAL_VZX_EXIT%"=="0" (
   echo.
-  echo Edit SIGNAL_VZX_EXE in this launcher if your Steam library is elsewhere.
+  echo VZX exited with code %SIGNAL_VZX_EXIT%.
   pause
-  exit /b 1
 )
 
-start "Cathedral Engine V02" "%SIGNAL_VZX_EXE%" -data_path "%~dp0vzx" -state signal-cathedral-v02
+exit /b %SIGNAL_VZX_EXIT%
